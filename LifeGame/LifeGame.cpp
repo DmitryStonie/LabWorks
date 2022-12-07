54a907822442113eab72299777ae493fad6a3a70
DmitryStonie <dmitrystonie@gmail.com>
2022-12-07 17:25:20 +0700

    Добавил примеров интересных миров

--- LifeGame/LifeGame.cpp
+++ LifeGame/LifeGame.cpp
@@ -11,6 +11,11 @@ int main(int argc, char* argv[])
 	gf::GameField field;
 	gf::ArgsContainer arguments(argc, argv);
 	arguments.gameFieldInitialization(field);
-	field.run();
+	console::Console console;
+	console.printMap(field.return_map(), 30, 30);
+	for (int i = 0; i < 100; i++) {
+		field.iterate(1);
+	}
+	//field.run();
 	return 0;
 }
