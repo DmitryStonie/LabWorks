2c43ee91bee4d560a928e9acb67d8c5052084d41
DmitryStonie <dmitrystonie@gmail.com>
2022-12-12 17:27:47 +0700

    Почти написал парсер заголовка, он не работает.

--- SoundProcessor/SoundProcessor/Parser.cpp
+++ SoundProcessor/SoundProcessor/Parser.cpp
@@ -1,28 +1,91 @@
 #include "Parser.h"
-@include <iostream>
+#include <iostream>
 #include <cstdio>
+#include <string>
 
 parser::Header::Header() {
-
+	(*this).set_default_header();
 }
 
 parser::Header::Header(std::string filename) {
-	FILE* input_file;
-	errno_t error;
-	error = fopen_s(&input_file, "severe_tire_damage.wav", "rb");
-	if (error) {
-		std::cerr << "Unable to open file, error " << error << '\n';
+	(*this).set_default_header();
+	FILE* input_file = fopen( "severe_tire_damage.wav", "rb");
+	if (input_file == NULL) {
+		std::cerr << "Unable to open file, error \n";
+		return;
 	}
 	//default
-	char buf[44];
+	char buf_char[HEADER_SIZE];
+	std::string buffer, buffer2;
 	int read_elem = 0;
-	read_elem = fread_s(buf, sizeof(char), HEADER_SIZE, sizeof(char), HEADER_SIZE, input_file);
-	if (read_elem != HEADER_SIZE) {
+	read_elem = fread(buf_char, 1, HEADER_SIZE, input_file);
+	buffer = read_elem;
+	if (sizeof(buffer) != HEADER_SIZE) {
 		std::cerr << "Header is corrupted.\n";
 	}
-
+	else {
+		chunkId = buffer.substr(0, 4);
+		buffer2 = buffer.substr(4, 4);
+		chunkSize = stoi(buffer2);
+		format = buffer.substr(8, 4);
+		subchunk1Id = buffer.substr(12, 4);
+		buffer2 = buffer.substr(16, 4);
+		subchunk1Size = stoi(buffer2);
+		buffer2 = buffer.substr(20, 2);
+		audioFormat = stoi(buffer2);
+		buffer2 = buffer.substr(22, 2);
+		numChannels = stoi(buffer2);
+		buffer2 = buffer.substr(24, 4);
+		sampleRate = stoi(buffer2);
+		buffer2 = buffer.substr(28, 4);
+		byteRate = stoi(buffer2);
+		buffer2 = buffer.substr(32, 2);
+		blockAlign = stoi(buffer2);
+		buffer2 = buffer.substr(34, 2);
+		bitsPerSample = stoi(buffer2);
+		subchunk2Id = buffer.substr(36, 4);
+		buffer2 = buffer.substr(40, 4);
+		subchunk2Size = stoi(buffer2);
+		is_correct = (*this).iscorrect();
+		if (is_correct == INCORRECT) {
+			std::cerr << "Header is not a WAV header. Can't use this file\n";
+		}
+	}
 }
 
 parser::Header::~Header() {
 
+}
+
+void parser::Header::set_default_header() {
+	is_correct = CORRECT;
+	chunkId = RIFF_CHAIN;
+	chunkSize = HEADER_SIZE - 4;	//data part is empty	
+	format = WAVE_FORMAT;
+	subchunk1Id = FMT_SUBCHAIN;
+	subchunk1Size = PCM_SUBCHUNK_SIZE;	//for PCM
+	audioFormat = PCM_FORMAT;	//for PCM
+	numChannels = DEFAULT_CHANNELS;
+	sampleRate = DEFAULT_RATE;
+	bitsPerSample = DEFAULT_BITS_PER_SAMPLE;
+	byteRate = sampleRate * numChannels * bitsPerSample / 8;	// 1 byte = 8 bits
+	blockAlign = numChannels * bitsPerSample / 8;
+	subchunk2Id = DATA_SUBCHAIN;
+	subchunk2Size = 0;	//data part is empty
+}
+
+const bool parser::Header::iscorrect() {
+	if (chunkId == RIFF_CHAIN && format == WAVE_FORMAT && subchunk1Id == FMT_SUBCHAIN
+		&& subchunk2Id == DATA_SUBCHAIN && sampleRate == DEFAULT_RATE
+		&& bitsPerSample == DEFAULT_BITS_PER_SAMPLE && numChannels == DEFAULT_CHANNELS
+		&& audioFormat == PCM_FORMAT && subchunk1Size == PCM_SUBCHUNK_SIZE) {
+		return CORRECT;
+	}
+	else {
+		return INCORRECT;
+	}
+}
+
+const bool parser::Header::return_correctness() {
+	return is_correct;
 }
--- SoundProcessor/SoundProcessor/Parser.h
+++ SoundProcessor/SoundProcessor/Parser.h
@@ -3,23 +3,38 @@
 
 namespace parser {
 	const int HEADER_SIZE = 44;
+	const int DEFAULT_RATE = 44100;
+	const int DEFAULT_CHANNELS = 1;
+	const int DEFAULT_BITS_PER_SAMPLE = 16;
+	const int PCM_SUBCHUNK_SIZE = 16;
+	const int PCM_FORMAT = 1;
+	const std::string RIFF_CHAIN = "RIFF";
+	const std::string WAVE_FORMAT = "WAVE";
+	const std::string FMT_SUBCHAIN = "fmt ";
+	const std::string DATA_SUBCHAIN = "data";
+	const bool CORRECT = 0;
+	const bool INCORRECT = 1;
 	class Header {
-		char chunkId[4];					//for "RIFF" symbols
-		long long chunkSize;				//filesize - 8
-		char format[4];						//for "WAVE" symbols
-		char subchunk1Id[4];				//for "fmt " symbols
+		bool is_correct;
+		std::string chunkId;				//for "RIFF" symbols
+		unsigned long chunkSize;			//filesize - 8
+		std::string format;					//for "WAVE" symbols
+		std::string subchunk1Id;			//for "fmt " symbols
 		unsigned long subchunk1Size;		//filesize - 16
-		short int audioFormat;				//1 for PCM - linear
-		short int numChannels;				//mono = 1
+		unsigned short audioFormat;			//1 for PCM - linear
+		unsigned short numChannels;			//mono = 1
 		unsigned long sampleRate;			//44100 Hz
 		unsigned long byteRate;				// sampleRate * numChannels * bitsPerSample/8
-		short int blockAlign;				//numChannels * bitsPerSample/8   - bytes for 1 sample
-		short int bitsPerSample;			//sound depth
-		char subchunk2Id[4];				//for "data symbols"
-		unsigned long sunchunk2Size;		//bites in data section
+		unsigned short blockAlign;			//numChannels * bitsPerSample/8   - bytes for 1 sample
+		unsigned short bitsPerSample;		//sound depth
+		std::string subchunk2Id;			//for "data symbols"
+		unsigned long subchunk2Size;		//bites in data section
 	public:
 		Header();
 		Header(std::string filename);
 		~Header();
+		void set_default_header();
+		const bool iscorrect();
+		const bool return_correctness();
 	};
 }
